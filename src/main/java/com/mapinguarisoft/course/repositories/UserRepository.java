941533d9f97fca42adc3688d688a1beb60c00c68
FrancoBrasil <fcbs.web@gmail.com>
2020-05-13 15:36:39 -0300

    Service layer, component registration

--- src/main/java/com/mapinguarisoft/course/repositories/UserRepository.java
+++ src/main/java/com/mapinguarisoft/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mapinguarisoft.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.mapinguarisoft.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
