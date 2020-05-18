3158d997054d72186d4ca48de128c3690a2d3619
FrancoBrasil <fcbs.web@gmail.com>
2020-05-18 10:30:18 -0300

    Exception handling - findById

--- src/main/java/com/mapinguarisoft/course/services/UserService.java
+++ src/main/java/com/mapinguarisoft/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mapinguarisoft.course.entities.User;
 import com.mapinguarisoft.course.repositories.UserRepository;
+import com.mapinguarisoft.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
